2b91858d0476f8f6f6dc442bc1ad62702859c035
Gemma Hassan <gemmaehassan@gmail.com>
2021-01-05 20:37:56 +0000

    Checking env

--- src/shared/http-common.js
+++ src/shared/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL: window.location.hostname.includes('herokuapp') ? 'https://msc-nursery-server.herokuapp.com/' : 'http://localhost:8080/',
   headers: {
     'Content-type': 'application/JSON',
   }
