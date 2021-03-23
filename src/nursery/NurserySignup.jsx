220b54cf2e839f5276a839818a6d6dfe9f7f5e36
Gemma Hassan <gemmaehassan@gmail.com>
2021-03-23 14:36:37 +0000

    Adds dynamic signup page for each nursery

--- src/nursery/NurserySignup.jsx
+++ src/nursery/NurserySignup.jsx
@@ -1,11 +1,13 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {IonContent, IonPage} from '@ionic/react';
-import {Button, Form, Input,} from "antd";
+import {Button, Form} from "antd";
 import NurseryDataService from "../services/nursery";
 import Nav from "../public/Nav";
+import {useParams} from "react-router-dom";
+import {CirclePicker} from 'react-color';
+
 
 const NurserySignup = () => {
-
   // TO DO: on submit, create nursery
   // create user? separate?
   // add admin role to db
@@ -13,43 +15,57 @@ const NurserySignup = () => {
   // address of nursery to be used in google map based on user's location, shows nearest nurseries
   // only northern ireland postcodes
   const [image, setImage] = useState();
+  const [nursery, setNursery] = useState();
+  const [color, setColor] = useState();
+  const [signupSuccessful, setSignupSuccessful] = useState(false);
 
-  const handleSignup = ({
-                          name,
-                          contactName,
-                          email,
-                          phone,
-                          addressLine1,
-                          addressLine2,
-                          town,
-                          county,
-                          postcode,
-                        }) => {
+  const colors = [
+    "#e91e63",
+    "#9c27b0",
+    "#673ab7",
+    "#3f51b5",
+    "#2196f3",
+    "#00bcd4",
+    "#4caf50",
+    "#8bc34a",
+    "#cddc39",
+    "#ff9800",
+    "#607d8b"];
+
+  const {nurseryId} = useParams();
+
+  useEffect(() => {
+    console.log("calling useEffect");
+    getNursery();
+    console.log("nursery: ", nursery);
+  }, []);
+
+  const getNursery = () => {
+    NurseryDataService.get(nurseryId)
+      .then(response => {
+        setNursery(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const handleSignup = () => {
     if (!image) {
       console.error('No image supplied. Need to add validation for this.');
       return;
     }
-    const formData = new FormData();
-    console.log("formData: ", formData);
-    formData.append('image', image, image.name);
-    formData.append('name', name);
-    formData.append('contactName', contactName);
-    formData.append('email', email);
-    formData.append('phone', phone);
-    formData.append('addressLine1', addressLine1);
-    formData.append('addressLine2', addressLine2);
-    formData.append('town', town);
-    formData.append('county', county);
-    formData.append('postcode', postcode);
 
-    console.log("append: ", formData);
-    NurseryDataService.signup(
-      formData
-    ).then(
-      response => {
-        console.log(response.data);
-        console.log("signup successful");
-      })
+    console.log("color: ", color);
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    formData.append('color', color);
+
+    NurseryDataService.signup(nurseryId, formData)
+      .then(
+        response => {
+          setSignupSuccessful(true);
+        })
       .catch(e => {
           const resMessage =
             (e.response &&
@@ -61,108 +77,82 @@ const NurserySignup = () => {
       );
   };
 
+  const handleColorChange = color => {
+    setColor(color.hex);
+  }
+
+  const getLayout = () => {
+    if (nursery) {
+      if (nursery.pending === 2) {
+        return (
+          <>already signed up</>
+        );
+      } else {
+        return (
+          <>
+            Hi, welcome to Nursery Journal. To finish creating an account for {nursery.name}, please upload an image
+            of
+            your nursery and select a colour for your page branding.
+
+            <div
+              style={{
+                'display': 'flex',
+                'flexDirection': 'column',
+                'padding': '50px',
+                width: '100%',
+                'maxWidth': '600px'
+              }}>
+              <Form
+                name="basic"
+                initialValues={{remember: true}}
+                onFinish={handleSignup}
+              >
+                <Form.Item
+                  name="image"
+                  label="Upload an image of your nursery"
+                >
+                  <input
+                    name="image" // name of input field or fieldName simply
+                    enctype="multipart/form-data"
+                    type="file"
+                    onChange={(event) => {
+                      // setState method with event.target.files[0] as argument
+                      setImage(event.target.files[0]);
+                    }}
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <CirclePicker
+                    colors={colors}
+                    onChangeComplete={handleColorChange}
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+          </>
+        );
+      }
+    } else {
+      return (
+        <>Nothing to see here</>
+      );
+    }
+  }
+
   return (
     <>
       <IonPage>
         <Nav/>
         <IonContent>
-          Please enter your nursery's details below. You will then receive an email with details on how to set up your
-          account.
-
-          <div
-            style={{
-              'display': 'flex',
-              'flexDirection': 'column',
-              'padding': '50px',
-              width: '100%',
-              'maxWidth': '600px'
-            }}>
-            <Form
-              name="basic"
-              initialValues={{remember: true}}
-              onFinish={handleSignup}
-            >
-              <Form.Item
-                label="Nursery Name"
-                name="name"
-                rules={[{required: true, message: 'Please input the nursery name!'}]}
-              >
-                <Input/>
-              </Form.Item>
-
-              <Form.Item
-                label="Contact Name"
-                name="contactName"
-                rules={[{required: true, message: 'Please add a contact name!'}]}
-              >
-                <Input/>
-              </Form.Item>
-
-              <Form.Item
-                label="Email"
-                name="email"
-                rules={[{required: true, message: 'Please add an email address!'}]}
-              >
-                <Input/>
-              </Form.Item>
-
-              <Form.Item
-                label="Phone Number"
-                name="phone"
-                rules={[{required: true, message: 'Please add a phone number'}]}
-              >
-                <Input placeholder="Phone Number"/>
-              </Form.Item>
-
-              <Form.Item
-                label="Address"
-                name="addressLine1"
-                rules={[{required: true, message: 'Please add an address!'}]}
-              >
-                <Input placeholder="Line 1"/>
-              </Form.Item>
-
-              <Form.Item
-                name="addressLine2"
-              >
-                <Input placeholder="Line 2"/>
-              </Form.Item>
-
-              <Form.Item
-                name="town"
-                rules={[{required: true, message: 'Please add a town!'}]}
-              >
-                <Input placeholder="Town"/>
-              </Form.Item>
-
-              <Form.Item
-                name="postcode"
-                rules={[{required: true, message: 'Please add a postcode!'}]}
-              >
-                <Input placeholder="Postcode"/>
-              </Form.Item>
-
-              <Form.Item
-                name="image"
-                label="Upload an image of your nursery"
-              >
-                <input
-                  name="image" // name of input field or fieldName simply
-                  enctype="multipart/form-data"
-                  type="file"
-                  onChange={(event) => {
-                    // setState method with event.target.files[0] as argument
-                    setImage(event.target.files[0]);
-                  }}
-                />
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
+          {getLayout()}
+          {signupSuccessful && (
+            <>Thank you! You can now use Nursery Journal</>
+          )}
         </IonContent>
       </IonPage>
     </>
--- src/public/signup/NurseryContact.jsx
+++ src/public/signup/NurseryContact.jsx
@@ -6,12 +6,35 @@ import Nav from "../Nav";
 
 const NurseryContact = () => {
 
+  const [showSuccess, setShowSuccess] = useState(false);
   // TO DO: on submit, create nursery
   // create user? separate?
   // add admin role to db
   // admin should be able to add staff, children and carers - generate usernames and passwords
   // address of nursery to be used in google map based on user's location, shows nearest nurseries
   // only northern ireland postcodes
+  const apiKey = 'aDUOicMHl0-6XpwvlhUH4w30713';
+
+  function debounce(func, wait) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        timeout = null;
+        func.apply(context, args);
+      }, wait);
+    };
+  }
+
+  const handlePostcodeChange = event => {
+      fetch(`https://api.getAddress.io/find/${event.target.value}?api-key=${apiKey}`)
+        .then(res => res.json())
+        .then(response => {
+          console.log(response);
+        })
+        .catch(console.log)
+  }
 
   const handleSignup = ({
                           name,
@@ -22,22 +45,28 @@ const NurseryContact = () => {
                           addressLine2,
                           town,
                           county,
-                          postcode,
+                          postcode
                         }) => {
-    NurseryDataService.signup(
-      name,
-      contactName,
-      email,
-      phone,
-      addressLine1,
-      addressLine2,
-      town,
-      county,
-      postcode,
+    const url = name.replace(/\s+/g, '');
+    let data = {
+      name: name,
+      contactName: contactName,
+      email: email,
+      phone: phone,
+      addressLine1: addressLine1,
+      addressLine2: addressLine2,
+      town: town,
+      county: county,
+      postcode: postcode,
+      url: url
+    };
+    NurseryDataService.contact(
+      data
     ).then(
       response => {
         console.log(response.data);
         console.log("signup successful");
+        setShowSuccess(true);
       })
       .catch(e => {
           const resMessage =
@@ -55,88 +84,106 @@ const NurseryContact = () => {
       <IonPage>
         <Nav/>
         <IonContent>
-          If you are interested in signing your nursery up for this service, please enter your nursery name and an administrator's contact details below. Someone from our team will be in touch within 24 hours to discuss our plans.
+          {!showSuccess && (
+            <>
+              If you are interested in signing your nursery up for this service, please enter your nursery name and an
+              administrator's contact details below. Someone from our team will be in touch within 24 hours to discuss
+              our
+              plans.
 
-          <div
-            style={{
-              'display': 'flex',
-              'flexDirection': 'column',
-              'padding': '50px',
-              width: '100%',
-              'maxWidth': '600px'
-            }}>
-            <Form
-              name="basic"
-              initialValues={{remember: true}}
-              onFinish={handleSignup}
-            >
-              <Form.Item
-                label="Nursery Name"
-                name="name"
-                rules={[{required: true, message: 'Please input the nursery name!'}]}
-              >
-                <Input/>
-              </Form.Item>
+              <div
+                style={{
+                  'display': 'flex',
+                  'flexDirection': 'column',
+                  'padding': '50px',
+                  width: '100%',
+                  'maxWidth': '600px'
+                }}>
+                <Form
+                  name="basic"
+                  initialValues={{remember: true}}
+                  onFinish={handleSignup}
+                >
+                  <Form.Item
+                    label="Nursery Name"
+                    name="name"
+                    rules={[{required: true, message: 'Please input the nursery name!'}]}
+                  >
+                    <Input/>
+                  </Form.Item>
 
-              <Form.Item
-                label="Contact Name"
-                name="contactName"
-                rules={[{required: true, message: 'Please add a contact name!'}]}
-              >
-                <Input/>
-              </Form.Item>
+                  <Form.Item
+                    label="Contact Name"
+                    name="contactName"
+                    rules={[{required: true, message: 'Please add a contact name!'}]}
+                  >
+                    <Input/>
+                  </Form.Item>
 
-              <Form.Item
-                label="Email"
-                name="email"
-                rules={[{required: true, message: 'Please add an email address!'}]}
-              >
-                <Input/>
-              </Form.Item>
+                  <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[{required: true, message: 'Please add an email address!'}]}
+                  >
+                    <Input/>
+                  </Form.Item>
 
-              <Form.Item
-                label="Phone Number"
-                name="phone"
-                rules={[{required: true, message: 'Please add a phone number'}]}
-              >
-                <Input placeholder="Phone Number"/>
-              </Form.Item>
+                  <Form.Item
+                    label="Phone Number"
+                    name="phone"
+                    rules={[{required: true, message: 'Please add a phone number'}]}
+                  >
+                    <Input placeholder="Phone Number"/>
+                  </Form.Item>
 
-              <Form.Item
-                label="Nursery Address"
-                name="addressLine1"
-                rules={[{required: true, message: 'Please add an address!'}]}
-              >
-                <Input placeholder="Line 1"/>
-              </Form.Item>
+                  <Form.Item
+                    label="Nursery Address"
+                    name="addressLine1"
+                    rules={[{required: true, message: 'Please add an address!'}]}
+                  >
+                    <Input placeholder="Line 1"/>
+                  </Form.Item>
 
-              <Form.Item
-                name="addressLine2"
-              >
-                <Input placeholder="Line 2"/>
-              </Form.Item>
+                  <Form.Item
+                    name="addressLine2"
+                  >
+                    <Input placeholder="Line 2"/>
+                  </Form.Item>
 
-              <Form.Item
-                name="town"
-                rules={[{required: true, message: 'Please add a town!'}]}
-              >
-                <Input placeholder="Town"/>
-              </Form.Item>
+                  <Form.Item
+                    name="town"
+                    rules={[{required: true, message: 'Please add a town!'}]}
+                  >
+                    <Input placeholder="Town"/>
+                  </Form.Item>
 
-              <Form.Item
-                name="postcode"
-                rules={[{required: true, message: 'Please add a postcode!'}]}
-              >
-                <Input placeholder="Postcode"/>
-              </Form.Item>
+                  <Form.Item
+                    name="county"
+                    rules={[{required: true, message: 'Please add a county!'}]}
+                  >
+                    <Input placeholder="County"/>
+                  </Form.Item>
 
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
+                  <Form.Item
+                    name="postcode"
+                    rules={[{required: true, message: 'Please add a postcode!'}]}
+                  >
+                    <Input placeholder="Postcode"
+                    onChange={debounce=(handlePostcodeChange, 200)}/>
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                      Submit
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </div>
+            </>
+          )}
+          {showSuccess && (
+            <p>Thank you. Someone will be in touch soon to set up your account.</p>
+          )}
         </IonContent>
       </IonPage>
     </>
