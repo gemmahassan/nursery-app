b27da7f5fc15fc0fca550dbf992b6494aeb59b40
Gemma Hassan <gemmaehassan@gmail.com>
2021-03-24 22:07:00 +0000

    Adds an error message when username or password is invalid

--- src/nursery/components/Login.jsx
+++ src/nursery/components/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Input,
   Form,
@@ -9,6 +9,8 @@ import AuthService from "../../services/auth";
 import {IonContent, IonPage} from "@ionic/react";
 
 const Login = (props) => {
+  const [loginFailed, setLoginFailed] = useState(false);
+
   const handleLogin = ({username, password}) => {
     AuthService.login(username, password).then(
       response => {
@@ -17,52 +19,52 @@ const Login = (props) => {
         window.location.reload();
       })
       .catch(e => {
-        const resMessage =
-          (e.response &&
-            e.response.data &&
-            e.response.data.message) ||
-          e.message ||
-          e.toString();
-      }
-    );
+          setLoginFailed(true);
+        }
+      );
   };
 
   return (
     <IonPage>
       <IonContent>
-        <div style={{'display': 'flex', 'flexDirection': 'column', 'padding': '50px', width: '100%', 'maxWidth': '600px'}}>
-        <h1>Login</h1>
-            <Form
-              name="basic"
-              initialValues={{ remember: true }}
-              onFinish={handleLogin}
+        <div
+          style={{'display': 'flex', 'flexDirection': 'column', 'padding': '50px', width: '100%', 'maxWidth': '600px'}}>
+          <h1>Login</h1>
+          <Form
+            name="basic"
+            initialValues={{remember: true}}
+            onFinish={handleLogin}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{required: true, message: 'Please input your username!'}]}
             >
-              <Form.Item
-                label="Username"
-                name="username"
-                rules={[{ required: true, message: 'Please input your username!' }]}
-              >
-                <Input />
-              </Form.Item>
+              <Input/>
+            </Form.Item>
 
-              <Form.Item
-                label="Password"
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
-              >
-                <Input.Password />
-              </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{required: true, message: 'Please input your password!'}]}
+            >
+              <Input.Password/>
+            </Form.Item>
 
-              <Form.Item  name="remember" valuePropName="checked">
-                <Checkbox>Remember me</Checkbox>
-              </Form.Item>
-              <Form.Item >
+            <Form.Item name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
 
+            {loginFailed && (
+              <p>Incorrect username or password</p>
+            )}
+
+            <Form.Item>
               <Button type="primary" htmlType="submit">
                 Submit
               </Button>
-              </Form.Item>
-            </Form>
+            </Form.Item>
+          </Form>
         </div>
       </IonContent>
     </IonPage>
